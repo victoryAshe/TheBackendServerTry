70c2a36b730f49d069ce1aee0ccdb3891aac10da
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2024-08-01 16:39:13 +0900

    Feat: 추가 0. 시작하기
    - 추가: [BackendManager] 뒤끝 초기화

--- Assets/Scripts/BackendManager.cs
+++ Assets/Scripts/BackendManager.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 뒤끝 SDK namespace
+using BackEnd;
+
 public class BackendManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
+        var bro = Backend.Initialize(); // 뒤끝 초기화
 
-    // Update is called once per frame
-    void Update()
-    {
+        //뒤끝 초기화에 대한 응답값
+        if (bro.IsSuccess())
+        {
+            Debug.Log($"초기화 성공: {bro}"); // statusCode 204 Success
+        }
+        else
+        {
+            Debug.LogError($"초기화 실패: {bro}"); // statusCode  4XX Error
+        }
         
     }
 }
